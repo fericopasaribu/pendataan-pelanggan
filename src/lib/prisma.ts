82dd53dcfa60d72a9fc6acb428884e8149323ed9
fericopasaribu <fopsoft@gmail.com>
2025-07-13 02:47:27 +0700

    update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,9 +5,7 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+  globalForPrisma.prisma ?? new PrismaClient({ log: ["error"] });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
